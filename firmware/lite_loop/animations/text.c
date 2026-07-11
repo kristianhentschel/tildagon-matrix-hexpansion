84bdb87111eb0e46f39c5c7245c2b506659d081c
Kristian Hentschel <kristian@kristianhentschel.com>
2026-07-11 16:39:19 +0100

    Text mode, two fixed-width fonts, offset set in registers

--- firmware/lite_loop/animations/text.c
+++ firmware/lite_loop/animations/text.c
@@ -2,51 +2,71 @@
 #include "leds.h"
 #include <stdbool.h>
 
-#define TEXT_COLS 1024
-static uint8_t image[TEXT_COLS];
+#define blit_pixel uint8_t
+#include "fonts/blit16.h"
+#include "fonts/blit32.h"
+
+#define OFFSCREEN_PADDING 6 // should be the largest width of one character in any font
+#define TEXT_BUFFER_WIDTH (18 + 2 * OFFSCREEN_PADDING) // larger than needed because blit font doesn't draw partial character
+#define TEXT_BUFFER_HEIGHT 10 // larger than display to cut off descenders in blit32 rather than ascenders
+static uint8_t buffer[TEXT_BUFFER_WIDTH * TEXT_BUFFER_HEIGHT];
 static bool init = false;
 
+// Activate text mode by writing 0x03 to animation type register (0x37)
+
+// this reinterprets the 'direct control' registers as ascii characters (zero terminated)
+// 0x50 direct control options (must be zero)
+// 0x51-0xEC ASCII text
+
+// Additional configuration:
+// 0x38 (2 bytes) 16-bit offset in pixels
+// 0x3A (1 byte) flags
+// - 1:0 font (00 = blit16, 01 = blit32, 10-11 = reserved)
+// - [not implemented] 2   flip (0 = baseline outer, 1 = baseline inner)
+// - [not implemented] 6:3 reserved
+// - [not implemented] 7   ended (= text is no longer visible in this segment due to offset)
+
 void animation_text(core_registers_t *core_registers, matrix_pwm_t *frame_buffer) {
-  // offset = data column currently shown in the right most display column
-  static uint32_t offset = 0;
-  const uint32_t length = TEXT_COLS + LEDS_GRID_COLS;
-
-  // Initialise image with garbage data
-  if (!init) {
-    for (uint32_t i = 0; i < TEXT_COLS; i++) {
-      image[i] = i % 255;
-    }
-    init = true;
-
-    for (int i = 0; i < LEDS_GRID_ROWS; i++) {
-      for (int j = 0; j < LEDS_GRID_COLS; j++) {
-        int pos = leds_grid_indices[i * LEDS_GRID_COLS + j];
-        frame_buffer[pos] = 255;
-        Delay_Ms(5);
-      }
+  uint16_t offset = (core_registers->animation_options_1[0]) << 8 | (core_registers->animation_options_1[1]);
+  uint8_t flags = core_registers->animation_options_1[2];
+  char *text = core_registers->direct_control_data;
+  uint8_t len = 0;
+  for (len = 0; len < sizeof(core_registers->direct_control_data); len++) {
+    if (text[len] == '\0') {
+      break;
     }
   }
+  
+  // TODO interpret flip flag and set ended? flag
+  
+  // Render text (this applies the offset too)
+  for (int i = 0; i < sizeof(buffer); i++) {
+    buffer[i] = 0;
+  }
+  
+  // offset = 0 means the text starts in the first off-screen column on the right edge
+  int startX = LEDS_GRID_COLS + OFFSCREEN_PADDING - offset;
+  switch (flags & 0x03) {
+    case 0x00:
+      blit16_TextNExplicit(buffer, 255, 1, TEXT_BUFFER_WIDTH, TEXT_BUFFER_HEIGHT, 0, startX, 2, len, text);
+      break;
+    case 0x01:
+    default:
+      blit32_TextNExplicit(buffer, 255, 1, TEXT_BUFFER_WIDTH, TEXT_BUFFER_HEIGHT, 0, startX, 1, len, text);
+  }
 
+  // Copy rendered pixels into frame buffer
+  // TODO this step and the `buffer` may be unneccessary but then text rendering would have to be taken out of blit*.h
   for (int i = 0; i < LEDS_GRID_COLS; i++) {
-    int32_t col = i + offset - LEDS_GRID_COLS;
-    uint8_t col_data = 0x00;
-    if (col >= 0 && col < TEXT_COLS) {
-      col_data = image[col];
-    }
-
     for (int j = 0; j < LEDS_GRID_ROWS; j++) {
-      int pos = leds_grid_indices[i + j * LEDS_GRID_COLS];
-      if (pos > LEDS_COUNT) continue;
-
-      if (j == 0) {
-        // Ignore top row because we only have an 8-bit wide buffer
-        frame_buffer[pos] = 0;
-      } else {
-        frame_buffer[pos] = (col_data & (1 << (8 - j))) ? 255 : 0;
+      uint8_t index = leds_grid_indices[i + j * LEDS_GRID_COLS];
+      if (index > LEDS_COUNT) {
+        continue;
       }
+
+      frame_buffer[index] = buffer[(i + OFFSCREEN_PADDING) + j * TEXT_BUFFER_WIDTH];
     }
   }
 
-  offset = (offset + 1) % length;
-  Delay_Ms(50);
+  Delay_Ms(10);
 }
--- firmware/shared/core_registers.h
+++ firmware/shared/core_registers.h
@@ -2,50 +2,50 @@
 #define CORE_REGISTERS_H
 #include <stdint.h>
 
-#define CORE_REGISTERS_VERSION 0x00000001
+#define CORE_REGISTERS_VERSION 0x20260711
 
 typedef struct core_registers {
   // 0x00
-  uint32_t core_registers_version;
-  uint32_t time_seconds;
-  uint16_t time_milliseconds;
-  uint8_t current_slot;
-  uint8_t used_slots;
-  uint8_t _reserved_0[4];
+  volatile uint32_t core_registers_version;
+  volatile uint32_t time_seconds;
+  volatile uint16_t time_milliseconds;
+  volatile uint8_t current_slot;
+  volatile uint8_t used_slots;
+  volatile uint8_t _reserved_0[4];
 
   // 0x10
-  int16_t acc_x;
-  int16_t acc_y;
-  int16_t acc_z;
-  int16_t acc_scale;
-  int16_t gyro_x;
-  int16_t gyro_y;
-  int16_t gyro_z;
-  int16_t gyro_scale;
+  volatile int16_t acc_x;
+  volatile int16_t acc_y;
+  volatile int16_t acc_z;
+  volatile int16_t acc_scale;
+  volatile int16_t gyro_x;
+  volatile int16_t gyro_y;
+  volatile int16_t gyro_z;
+  volatile int16_t gyro_scale;
 
   // 0x20
-  int16_t mag_x;
-  int16_t mag_y;
-  int16_t mag_z;
-  int16_t mag_scale;
-  uint8_t _reserved_2[8];
+  volatile int16_t mag_x;
+  volatile int16_t mag_y;
+  volatile int16_t mag_z;
+  volatile int16_t mag_scale;
+  volatile uint8_t _reserved_2[8];
 
   // 0x30
-  uint8_t animation_brightness;
-  uint8_t animation_period;
-  uint8_t reserved_3[5];
-  uint8_t animation_type;         // 0x37
-  uint8_t animation_options_1[8]; // 0x38
+  volatile uint8_t animation_brightness;
+  volatile uint8_t animation_period;
+  volatile uint8_t reserved_3[5];
+  volatile uint8_t animation_type;         // 0x37
+  volatile uint8_t animation_options_1[8]; // 0x38
 
   // 0x40
-  uint8_t animation_options_2[16];
+  volatile uint8_t animation_options_2[16];
 
   // 0x50
-  uint8_t direct_control_options;
+  volatile uint8_t direct_control_options;
   
   // 0x51 - 0xEC
   // depending on setting of direct control options only a subset of these bytes will be used
-  uint8_t direct_control_data[156];
+  volatile uint8_t direct_control_data[156];
 
   // NB 0xFD is reserved as the command register to select the active page
 } core_registers_t;
